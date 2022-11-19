575533a990eb914c407ce041e94f910ad9653f22
Marco Ferraro <mantofer2000@gmail.com>
2022-11-19 16:14:46 -0600

    adding copunter

--- Proyecto/Version 0.3/pong.py
+++ Proyecto/Version 0.3/pong.py
@@ -17,7 +17,7 @@ class Pong:
         pygame.init()
 
         self.light_grey = (200, 200, 200)
-        self.bg_color = pygame.Color('grey12')
+        self.bg_color = pygame.Color(0, 0, 0)
 
         self.ball_speed_x = 7
         self.ball_speed_y = 7
@@ -38,6 +38,14 @@ class Pong:
 
         self.player_2 = pygame.Rect(SCREEN_WITDH - 20,  (SCREEN_HEIGHT / 2) -
                                     (PADELL_HEIGHT / 2),  PADDEL_WIDTH, PADELL_HEIGHT)
+
+        # text variables
+        self.player_1_score = 0
+        self.player_2_score = 0
+        self.game_font = pygame.font.Font("freesansbold.ttf", 32)
+
+        # score timer
+        self.score_time = True
 
         self.render_game()
 
@@ -70,13 +78,54 @@ class Pong:
             pygame.draw.aaline(self.screen, self.light_grey, (SCREEN_WITDH/2,
                                                               0), (SCREEN_WITDH/2, SCREEN_HEIGHT))
 
+            if self.score_time:
+                self.ball_restart()
+
+            player_1_text = self.game_font.render(
+                f"{self.player_1_score}", False, self.light_grey)
+
+            player_2_text = self.game_font.render(
+                f"{self.player_2_score}", False, self.light_grey)
+
+            self.screen.blit(player_1_text, (600, 20))
+            self.screen.blit(player_2_text, (660, 20))
+
             pygame.display.flip()
             self.clock.tick(60)
 
     def ball_restart(self):
+        number_render = None
+        current_time = pygame.time.get_ticks()
+
         self.ball.center = (SCREEN_WITDH / 2, SCREEN_HEIGHT / 2)
-        self.ball_speed_x *= random.choice((-1, 1))
-        self.ball_speed_y *= random.choice((-1, 1))
+
+        if current_time - self.score_time < 500:
+            number_render = self.game_font.render("3", False, self.light_grey)
+            self.screen.blit(number_render, (SCREEN_WITDH /
+                             2 - 10, SCREEN_HEIGHT / 2 - 60))
+        elif 500 <= current_time - self.score_time < 1000:
+            number_render = self.game_font.render("2", False, self.light_grey)
+            self.screen.blit(number_render, (SCREEN_WITDH /
+                             2 - 10, SCREEN_HEIGHT / 2 - 60))
+        elif 1000 <= current_time - self.score_time < 1500:
+            number_render = self.game_font.render("1", False, self.light_grey)
+            self.screen.blit(number_render, (SCREEN_WITDH /
+                             2 - 10, SCREEN_HEIGHT / 2 - 60))
+
+        if current_time - self.score_time < 1500:
+            self.ball_speed_x, self.ball_speed_y = 0, 0
+        else:
+            self.ball_speed_x = 7 * random.choice((-1, 1))
+            self.ball_speed_y = 7 * random.choice((-1, 1))
+            self.score_time = None
+
+    def player_1_scores(self):
+        self.score_time = pygame.time.get_ticks()
+        self.player_1_score += 1
+
+    def player_2_scores(self):
+        self.score_time = pygame.time.get_ticks()
+        self.player_2_score += 1
 
     def ball_animation(self):
 
@@ -86,27 +135,48 @@ class Pong:
         if self.ball.top <= 0 or self.ball.bottom >= SCREEN_HEIGHT:
             self.ball_speed_y *= -1
 
-        if self.ball.left <= 0 or self.ball.right >= SCREEN_WITDH:
-            self.ball_restart()
+        if self.ball.left <= 0:
+            self.player_2_scores()
+            # self.ball_restart()
 
-        if self.ball.colliderect(self.player_1) or self.ball.colliderect(self.player_2):
-            self.ball_speed_x *= -1
+        if self.ball.right >= SCREEN_WITDH:
+            self.player_1_scores()
+            # self.ball_restart()
+
+        if self.ball.colliderect(self.player_1):
+            if abs(self.ball.right - self.player_1.left) < 10:
+                self.ball_speed_x *= -1
+            elif abs(self.ball.bottom - self.player_1.top) < 10 and self.ball_speed_y > 0:
+                self.ball_speed_y *= -1
+            elif abs(self.ball.top - self.player_1.bottom) < 10 and self.ball_speed_y < 0:
+                self.ball_speed_y *= -1
+
+        if self.ball.colliderect(self.player_2):
+            if abs(self.ball.left - self.player_2.right) < 10:
+                self.ball_speed_x *= -1
+            elif abs(self.ball.bottom - self.player_2.top) < 10 and self.ball_speed_y > 0:
+                self.ball_speed_y *= -1
+            elif abs(self.ball.top - self.player_2.bottom) < 10 and self.ball_speed_y < 0:
+                self.ball_speed_y *= -1
 
     def player_1_animation(self):
         self.player_1.y += self.player_1_speed
 
         if self.player_1.top <= 0:
             self.player_1.top = 0
+
         if self.player_1.bottom >= SCREEN_HEIGHT:
             self.player_1.bottom = SCREEN_HEIGHT
 
     def player_2_ai(self):
         if self.player_2.top < self.ball.y:
             self.player_2.top += self.player_2_speed
+
         if self.player_2.bottom > self.ball.y:
             self.player_2.bottom -= self.player_2_speed
 
         if self.player_2.top <= 0:
             self.player_2.top = 0
+
         if self.player_2.bottom >= SCREEN_HEIGHT:
             self.player_2.bottom = SCREEN_HEIGHT
