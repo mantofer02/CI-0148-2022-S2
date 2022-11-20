defe56a1ce47b93489326a9b9dcbb52c02e2c06c
Marco Ferraro <mantofer2000@gmail.com>
2022-11-19 18:17:56 -0600

    adding buttons

--- Proyecto/Version 0.3/pong.py
+++ Proyecto/Version 0.3/pong.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from button import Button
 
 SCREEN_WITDH = 1280
 SCREEN_HEIGHT = 960
@@ -45,9 +46,59 @@ class Pong:
         self.game_font = pygame.font.Font("freesansbold.ttf", 32)
 
         # score timer
+
+        self.game_paused = True
         self.score_time = True
 
+        self.pvp_img = pygame.image.load("images/button_player-vs-player.png")
+        self.pvCPU_img = pygame.image.load("images/button_player-vs-cpu.png")
+        self.CPUvCPU_img = pygame.image.load("images/button_cpu-vs-cpu.png")
+
         self.render_game()
+
+    def display_menu(self):
+        self.screen.fill(self.bg_color)
+
+        pvp_button = Button(SCREEN_WITDH / 2 - 170,
+                            SCREEN_HEIGHT / 2 - 200, self.pvp_img, 1)
+
+        pvCPU_button = Button(SCREEN_WITDH / 2 - 143,
+                              SCREEN_HEIGHT / 2 - 100, self.pvCPU_img, 1)
+
+        CPUvCPU_button = Button(SCREEN_WITDH / 2 - 118,
+                                SCREEN_HEIGHT / 2, self.CPUvCPU_img, 1)
+
+        pvp_button.draw(self.screen)
+        pvCPU_button.draw(self.screen)
+        CPUvCPU_button.draw(self.screen)
+        pygame.display.flip()
+
+    def display_pong(self):
+        self.ball_animation()
+        self.player_1_animation()
+        self.player_2_ai()
+
+        self.screen.fill(self.bg_color)
+        pygame.draw.rect(self.screen, self.light_grey, self.player_1)
+        pygame.draw.rect(self.screen, self.light_grey, self.player_2)
+        pygame.draw.ellipse(self.screen, self.light_grey, self.ball)
+        pygame.draw.aaline(self.screen, self.light_grey, (SCREEN_WITDH/2,
+                                                          0), (SCREEN_WITDH/2, SCREEN_HEIGHT))
+
+        if self.score_time:
+            self.ball_restart()
+
+        player_1_text = self.game_font.render(
+            f"{self.player_1_score}", False, self.light_grey)
+
+        player_2_text = self.game_font.render(
+            f"{self.player_2_score}", False, self.light_grey)
+
+        self.screen.blit(player_1_text, (600, 20))
+        self.screen.blit(player_2_text, (660, 20))
+
+        pygame.display.flip()
+        self.clock.tick(60)
 
     def render_game(self):
         while True:
@@ -59,39 +110,22 @@ class Pong:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_DOWN:
                         self.player_1_speed += 7
-                    if event.key == pygame.K_UP:
+                    elif event.key == pygame.K_UP:
                         self.player_1_speed -= 7
+                    elif event.key == pygame.K_SPACE:
+                        self.game_paused = False
+                        self.score_time = pygame.time.get_ticks()
+
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_DOWN:
                         self.player_1_speed -= 7
-                    if event.key == pygame.K_UP:
+                    elif event.key == pygame.K_UP:
                         self.player_1_speed += 7
 
-            self.ball_animation()
-            self.player_1_animation()
-            self.player_2_ai()
-
-            self.screen.fill(self.bg_color)
-            pygame.draw.rect(self.screen, self.light_grey, self.player_1)
-            pygame.draw.rect(self.screen, self.light_grey, self.player_2)
-            pygame.draw.ellipse(self.screen, self.light_grey, self.ball)
-            pygame.draw.aaline(self.screen, self.light_grey, (SCREEN_WITDH/2,
-                                                              0), (SCREEN_WITDH/2, SCREEN_HEIGHT))
-
-            if self.score_time:
-                self.ball_restart()
-
-            player_1_text = self.game_font.render(
-                f"{self.player_1_score}", False, self.light_grey)
-
-            player_2_text = self.game_font.render(
-                f"{self.player_2_score}", False, self.light_grey)
-
-            self.screen.blit(player_1_text, (600, 20))
-            self.screen.blit(player_2_text, (660, 20))
-
-            pygame.display.flip()
-            self.clock.tick(60)
+            if self.game_paused:
+                self.display_menu()
+            else:
+                self.display_pong()
 
     def ball_restart(self):
         number_render = None
@@ -144,20 +178,10 @@ class Pong:
             # self.ball_restart()
 
         if self.ball.colliderect(self.player_1):
-            if abs(self.ball.right - self.player_1.left) < 10:
-                self.ball_speed_x *= -1
-            elif abs(self.ball.bottom - self.player_1.top) < 10 and self.ball_speed_y > 0:
-                self.ball_speed_y *= -1
-            elif abs(self.ball.top - self.player_1.bottom) < 10 and self.ball_speed_y < 0:
-                self.ball_speed_y *= -1
+            self.ball_speed_x *= -1
 
         if self.ball.colliderect(self.player_2):
-            if abs(self.ball.left - self.player_2.right) < 10:
-                self.ball_speed_x *= -1
-            elif abs(self.ball.bottom - self.player_2.top) < 10 and self.ball_speed_y > 0:
-                self.ball_speed_y *= -1
-            elif abs(self.ball.top - self.player_2.bottom) < 10 and self.ball_speed_y < 0:
-                self.ball_speed_y *= -1
+            self.ball_speed_x *= -1
 
     def player_1_animation(self):
         self.player_1.y += self.player_1_speed
